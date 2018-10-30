ec4e8223d90b91a6407012648efbd4bf54a255e6
semio <prairy.long@gmail.com>
2018-10-30 11:07:40 +0800

    update script
    - concurrent download
    - add retries and timeout
    - cleanup source dir first

--- etl/scripts/update_source.py
+++ etl/scripts/update_source.py
@@ -1,10 +1,41 @@
 # -*- coding: utf-8 -*-
 
+import glob
+import os
 import os.path as osp
 import requests as req
+from multiprocessing import Pool
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from functools import partial
 
 
 API_BASE = "https://unstats.un.org"
+
+
+def cleanup_source():
+    for f in glob.glob("../source/*csv"):
+        os.remove(f)
+
+# from https://www.peterbe.com/plog/best-practice-with-retries-with-requests
+def requests_retry_session(
+    retries=3,
+    backoff_factor=0.3,
+    status_forcelist=(500, 502, 504),
+    session=None,
+):
+    session = session or req.Session()
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 
 def api_path(api_base, *args):
@@ -22,21 +53,29 @@ def get_all_series():
     return j
 
 
+def download(v, csv_api):
+    code = v['code']
+
+    f = f'../source/{code}.csv'
+    csv = requests_retry_session().post(csv_api, data={'seriesCodes': code}, stream=True, timeout=60)
+
+    with open(f'../source/{code}.csv', 'wb') as f:
+        for chunk in csv.iter_content(chunk_size=1024):
+            f.write(chunk)
+            f.flush()
+
+
 def main():
-    j = get_all_series()
+    all_ser = get_all_series()
     csv_api = api_path(API_BASE, "/SDGAPI", "/v1/sdg/Series/DataCSV")
-    for v in j:
-        code = v['code']
 
-        f = f'../source/{code}.csv'
-        csv = req.post(csv_api, data={'seriesCodes': code}, stream=True)
+    download_ = partial(download, csv_api=csv_api)
 
-        with open(f'../source/{code}.csv', 'wb') as f:
-            for chunk in csv.iter_content(chunk_size=1024):
-                f.write(chunk)
-                f.flush()
+    with Pool(5) as p:
+        p.map(download_, all_ser)
 
 
 if __name__ == '__main__':
+    cleanup_source()
     main()
     print('Done.')
